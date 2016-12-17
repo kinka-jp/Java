946aec50f9e3dd7a773760810506b9bb8e5d30fa
tan kinka <lunwu.xiaoxia@gmail.com>
2016-12-17 18:04:19 +0900

    hello world

---
+++ src/org/lins/java/Java.java
@@ -0,0 +1,11 @@
+package org.lins.java;
+
+public class Java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("test");
+		System.out.println("Hello World!");
+	}
+
+}
