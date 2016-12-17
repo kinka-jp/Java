81768828ca28f52e67ed7fcb515f4179036d5f6b
tan kinka <lunwu.xiaoxia@gmail.com>
2016-12-18 00:00:56 +0900

    add a function.

--- src/org/lins/java/Java.java
+++ src/org/lins/java/Java.java
@@ -6,6 +6,11 @@ public class Java {
 		// TODO Auto-generated method stub
 		System.out.print("test");
 		System.out.println("Hello World!");
+		System.out.println("result:"+addition(3.0,5.6)+"");
+	}
+	
+	public static double addition(double a, double b){
+		return a+b;
 	}
 
 }
